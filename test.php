1cd0a60959c5cb902e196ceed8a380d33e51725d
truongvp9 <truongnm@vp9.vn>
2017-09-17 22:08:10 +0700

    add all files

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php   
+		$command = escapeshellcmd('python /home/vp9/public_html/test.py');
+		$output = shell_exec($command);
+            	echo "Query for:<br>";
+            	//$output = exec("sudo python test.py");
+	    	echo $output;
+?>

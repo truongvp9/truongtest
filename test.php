02afd5b0e83b596900f051b357b88936fd9601d3
truongvp9 <truongnm@vp9.vn>
2017-09-17 22:25:32 +0700

    echo welcome truong

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php   
 		echo "Fix bug";
+		echo "welcome truong";
 		$command = escapeshellcmd('python /home/vp9/public_html/test.py');
 		$output = shell_exec($command);
             	echo "Query for:<br>";
